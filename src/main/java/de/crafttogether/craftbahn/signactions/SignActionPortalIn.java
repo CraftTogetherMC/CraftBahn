c6e939a7220c5b1c217883c60edb07bbcaf05c7c
IrgendSoEinTyp <j0schlz@gmx.net>
2021-08-15 09:48:45 +0200

    Display error if sign can't be created

--- src/main/java/de/crafttogether/craftbahn/signactions/SignActionPortalIn.java
+++ src/main/java/de/crafttogether/craftbahn/signactions/SignActionPortalIn.java
@@ -1,11 +1,16 @@
 package de.crafttogether.craftbahn.signactions;
 
+import com.bergerkiller.bukkit.common.resources.SoundEffect;
+import com.bergerkiller.bukkit.common.utils.WorldUtil;
+import com.bergerkiller.bukkit.tc.Util;
 import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
 import com.bergerkiller.bukkit.tc.signactions.SignActionType;
 import com.bergerkiller.bukkit.tc.utils.SignBuildOptions;
 import de.crafttogether.craftbahn.util.Message;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
 
 public class SignActionPortalIn extends SignAction {
 
@@ -26,13 +31,14 @@ public class SignActionPortalIn extends SignAction {
     }
 
     @Override
-    public boolean build(SignChangeActionEvent event) {
-        String[] lines = event.getLines();
+    public boolean build(SignChangeActionEvent info) {
+        String[] lines = info.getLines();
         String status;
 
         // Validate third line
         if (lines[2].length() < 1) {
-            event.getPlayer().sendMessage(Message.format("&c\nPlease write a name for this portal on the third line"));
+            info.getPlayer().sendMessage(Message.format("&c\nPlease write a name for this portal on the third line"));
+            displayError(info);
             return false;
         }
 
@@ -43,11 +49,21 @@ public class SignActionPortalIn extends SignAction {
             status = "\nPortal-Exit was found";
 
         // Respond
-        SignBuildOptions opt = SignBuildOptions.create()
+        return SignBuildOptions.create()
             .setName("Portal-Entrance")
-            .setDescription("allow trains to travel between servers" + status);
+            .setDescription("allow trains to travel between servers" + status)
+            .handle(info.getPlayer());
+    }
 
-        return opt.handle(event.getPlayer());
+    private void displayError(SignActionEvent info) {
+        // When not successful, display particles at the sign to indicate such
+        BlockFace facingInv = info.getFacing().getOppositeFace();
+        Location effectLocation = info.getSign().getLocation()
+                .add(0.5, 0.5, 0.5)
+                .add(0.3 * facingInv.getModX(), 0.0, 0.3 * facingInv.getModZ());
+
+        Util.spawnDustParticle(effectLocation, 255.0, 255.0, 0.0);
+        WorldUtil.playSound(effectLocation, SoundEffect.EXTINGUISH, 1.0f, 2.0f);
     }
 
     private void onCartEnter(SignActionEvent info) {
--- src/main/java/de/crafttogether/craftbahn/signactions/SignActionPortalOut.java
+++ src/main/java/de/crafttogether/craftbahn/signactions/SignActionPortalOut.java
@@ -1,10 +1,15 @@
 package de.crafttogether.craftbahn.signactions;
 
+import com.bergerkiller.bukkit.common.resources.SoundEffect;
+import com.bergerkiller.bukkit.common.utils.WorldUtil;
+import com.bergerkiller.bukkit.tc.Util;
 import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
 import com.bergerkiller.bukkit.tc.utils.SignBuildOptions;
 import de.crafttogether.craftbahn.util.Message;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
 
 public class SignActionPortalOut extends SignAction {
 
@@ -17,20 +22,31 @@ public class SignActionPortalOut extends SignAction {
     public void execute(SignActionEvent info) { }
 
     @Override
-    public boolean build(SignChangeActionEvent event) {
-        String[] lines = event.getLines();
+    public boolean build(SignChangeActionEvent info) {
+        String[] lines = info.getLines();
 
         // Validate third line
         if (lines[2].length() < 1) {
-            event.getPlayer().sendMessage(Message.format("&c\nPlease write a name for this portal on the third line"));
+            info.getPlayer().sendMessage(Message.format("&c\nPlease write a name for this portal on the third line"));
+            displayError(info);
             return false;
         }
 
         // Respond
-        SignBuildOptions opt = SignBuildOptions.create()
+        return SignBuildOptions.create()
             .setName("Portal-Exit")
-            .setDescription("allow trains to travel between servers");
+            .setDescription("allow trains to travel between servers")
+            .handle(info.getPlayer());
+    }
 
-        return opt.handle(event.getPlayer());
+    private void displayError(SignActionEvent info) {
+        // When not successful, display particles at the sign to indicate such
+        BlockFace facingInv = info.getFacing().getOppositeFace();
+        Location effectLocation = info.getSign().getLocation()
+                .add(0.5, 0.5, 0.5)
+                .add(0.3 * facingInv.getModX(), 0.0, 0.3 * facingInv.getModZ());
+
+        Util.spawnDustParticle(effectLocation, 255.0, 255.0, 0.0);
+        WorldUtil.playSound(effectLocation, SoundEffect.EXTINGUISH, 1.0f, 2.0f);
     }
 }
