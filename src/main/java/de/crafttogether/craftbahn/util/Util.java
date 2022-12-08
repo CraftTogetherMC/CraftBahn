579fce7efdeebf786e5a04a2ad7722fd1f5dba5b
J0schlZ <j0schlz@gmx.net>
2022-12-08 13:33:43 +0100

    Added some debug-methods

--- src/main/java/de/crafttogether/craftbahn/util/Util.java
+++ src/main/java/de/crafttogether/craftbahn/util/Util.java
@@ -1,7 +1,12 @@
 package de.crafttogether.craftbahn.util;
 
+import de.crafttogether.CraftBahnPlugin;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -12,5 +17,27 @@ public class Util {
                 .distinct()
                 .filter(offlinePlayer -> offlinePlayer.getName().equalsIgnoreCase(name))
                 .collect(Collectors.toList()).get(0);
+    }
+
+    public static void debug(Component message, boolean broadcast) {
+        message = LegacyComponentSerializer.legacyAmpersand().deserialize("<gray><bold>[Debug]: </bold></gray><reset>").append(message);
+
+        // Broadcast to online players with permission
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (!CraftBahnPlugin.plugin.getConfig().getBoolean("debug") || !player.hasPermission("craftbahn.debug")) continue;
+            player.sendMessage(message);
+        }
+
+        CraftBahnPlugin.plugin.getLogger().info(PlainTextComponentSerializer.plainText().serialize(message));
+    }
+
+    public static void debug(String message, boolean broadcast) {
+        debug(Component.text(message), broadcast);
+    }
+    public static void debug(String message) {
+        debug(Component.text(message), false);
+    }
+    public static void debug(Component message) {
+        debug(message, false);
     }
 }
