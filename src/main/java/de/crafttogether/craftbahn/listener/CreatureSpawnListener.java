0f78816f1eba1b05ab4d0378272e907ed4c3c922
J0schlZ <j0schlz@gmx.net>
2022-12-30 23:59:02 +0100

    Remove EntityDeathEvent

--- src/main/java/de/crafttogether/craftbahn/listener/CreatureSpawnListener.java
+++ src/main/java/de/crafttogether/craftbahn/listener/CreatureSpawnListener.java
@@ -1,15 +1,11 @@
 package de.crafttogether.craftbahn.listener;
 
-import de.crafttogether.CraftBahnPlugin;
 import de.crafttogether.craftbahn.portals.Passenger;
 import de.crafttogether.craftbahn.portals.PortalHandler;
-import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 
 public class CreatureSpawnListener implements Listener {
 
@@ -20,13 +16,5 @@ public class CreatureSpawnListener implements Listener {
 
         if (passenger != null)
             PortalHandler.reEnterEntity(passenger, event);
-    }
-
-    @EventHandler
-    public void onDie(EntityDeathEvent event) {
-        if (event.getEntity().getType().equals(EntityType.VILLAGER)) {
-            Location loc = event.getEntity().getLocation();
-            CraftBahnPlugin.plugin.getLogger().warning("Villager died at " + loc.getX() + " " + loc.getY() + " " + loc.getZ());
-        }
     }
 }
